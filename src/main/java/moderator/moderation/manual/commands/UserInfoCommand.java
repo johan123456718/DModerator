3df555733272f7bc12d414c6745b2ec7da60dc0f
risbah <14286113+risbah@users.noreply.github.com>
2020-07-31 23:43:23 -0400

    Refactors userInfoCommand.java to display user avatar in thumbnail, to display roles as mentionables, and to display online status of user

--- src/main/java/moderator/moderation/manual/commands/UserInfoCommand.java
+++ src/main/java/moderator/moderation/manual/commands/UserInfoCommand.java
@@ -5,11 +5,10 @@ import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.*;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-
 import java.awt.*;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class UserInfoCommand extends Command {
 
@@ -18,7 +17,7 @@ public class UserInfoCommand extends Command {
     public UserInfoCommand(EventWaiter waiter){
         super.name = "user";
         super.help = "Get some information about a user";
-        super.aliases = new String[]{"u"};
+        super.aliases = new String[]{"u","U", "userinfo", "userInfo", "UserInfo"};
         super.category = new Category("Members");
         super.cooldown = 10;
         this.waiter = waiter;
@@ -27,27 +26,35 @@ public class UserInfoCommand extends Command {
     @Override
     protected void execute(CommandEvent event) {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        event.reply("Ok! Now, give me the name of a user. Like this @name");
+        if(event.getMessage().getMentionedMembers().size()==0){
+            event.reply("Please mention the user with the command. Try again!");
+        } else {
+            Member mentionedUser = event.getMessage().getMentionedMembers().get(0);
+            event.reply(userInfoEmbed(mentionedUser).build());
+        }
+    }
 
-        waiter.waitForEvent(GuildMessageReceivedEvent.class,
-            e -> e.getAuthor().equals(event.getAuthor()) && e.getChannel().equals(event.getChannel()),
-                e -> {
-                try {
-                    Member mentionUser = e.getMessage().getMentionedMembers().get(0);
-                    EmbedBuilder eb = new EmbedBuilder()
-                            .setColor(Color.magenta)
-                            .setThumbnail("http://pixelartmaker.com/art/fec737afdb065ed.png")
-                            .setDescription(mentionUser.getUser().getName() + " joined on " + mentionUser.getTimeJoined().format(fmt) + " :clock: ")
-                            .setAuthor("Information on " + mentionUser.getUser().getName(), "http://www.google.com", mentionUser.getUser().getAvatarUrl())
-                            .addField("Status on desktop:", mentionUser.getOnlineStatus(ClientType.DESKTOP).toString(), true)
-                            .addField("Nickname: ", mentionUser.getNickname() == null ? "No Nickname" : mentionUser.getNickname(), true);
-                    event.reply(eb.build());
-                    event.reply(event.getAuthor().getAsMention() + " there you go");
-                } catch (IndexOutOfBoundsException ex) {
-                    System.out.println("Exception Occured");
-                    event.reply("You need to provide the name as a mention.");
-                }
-            }, 15, TimeUnit.SECONDS, () -> event.reply("You did not give me a name to search. Try again.")); //Last 3 arguments specify how long to wait for the event and what to do if they never respond
+    private EmbedBuilder userInfoEmbed(Member member){
+        Color embedColor = member.getRoles().get(0).getColor();
+        String avatarUrl = member.getUser().getAvatarUrl();
+        String desc = "Joined: " + member.getTimeJoined().format(DateTimeFormatter.ofPattern("dd-MM-YYYY"));
+        String user = member.getEffectiveName();
+        String userTag = member.getUser().getAsTag();
+        String status = member.getOnlineStatus().name().replaceAll("_", " ");// modifies DO-NOT-DISTURBED
+        String roles = member.getRoles()
+                .stream()
+                .map(n -> String.valueOf(n.getAsMention()))
+                .collect(Collectors.joining(" "));
+
+
+        return new EmbedBuilder()
+                .setColor(embedColor)
+                .setThumbnail(avatarUrl)
+                .setDescription(desc)
+                .setAuthor(userTag, null, avatarUrl)
+                .addField("Nickname: ", user, true)
+                .addField("Status: ", status, true)
+                .addField("Roles: ", roles, false);
     }
 
 }
