9cd65b31b8d27b3fd326726133d75632e74847d7
johan123456718 <axel1234516@hotmail.com>
2020-08-04 15:49:49 +0200

    fixed the trigger problem

--- src/main/java/moderator/moderation/manual/commands/TriggerSwitchForAnti.java
+++ src/main/java/moderator/moderation/manual/commands/TriggerSwitchForAnti.java
@@ -6,16 +6,19 @@ import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import moderator.moderation.auto.filter.AntiSpamFilter;
 import net.dv8tion.jda.api.Permission;
 
+import java.util.List;
+
 public class TriggerSwitchForAnti extends Command {
     private final EventWaiter waiter;
     private final Permission[] requiredRoles = {Permission.MANAGE_ROLES, Permission.ADMINISTRATOR };
+    private List<Object> registeredListeners;
 
     public TriggerSwitchForAnti(EventWaiter waiter){
         super.name = "anti";
         super.help = "Triggers anti spam";
         super.aliases = new String[]{"a", "A"};
         super.category = new Command.Category("Admin");
-        super.cooldown = 10;
+        super.cooldown = 3;
         super.arguments = "[on, off]";
         super.userPermissions = requiredRoles;
         this.waiter = waiter;
@@ -27,10 +30,11 @@ public class TriggerSwitchForAnti extends Command {
         if(args[1].equals("on")){
             event.reply("Anti curse trigger: on");
             event.getJDA().addEventListener(new AntiSpamFilter());
+            registeredListeners = event.getJDA().getRegisteredListeners();
         }
         if (args[1].equals("off")){
             event.reply("Anti curse trigger: off");
-            event.getJDA().removeEventListener();
+            event.getJDA().removeEventListener(registeredListeners.get(2));
         }
     }
 }
